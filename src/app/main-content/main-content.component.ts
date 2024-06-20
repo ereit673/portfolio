48e130e1e37815a657d08e17c4fa7a9a9392096a
Alex Eremie <alex.eremie19@gmail.com>
2024-06-20 22:02:39 +0200

    set up project

---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [LandingPageComponent],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
