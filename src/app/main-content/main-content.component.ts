cafe69c11308d1c1adf707bcbe4de82ab22a91ac
Alex Eremie <alex.eremie19@gmail.com>
2024-06-22 11:20:49 +0200

    created component about

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent],
+  imports: [LandingPageComponent, AboutMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
