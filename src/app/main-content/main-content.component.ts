59e39a7628c166cd11053b87a982753eec03f553
Alex Eremie <alex.eremie19@gmail.com>
2024-06-23 12:42:10 +0200

    updated my skills component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, AboutMeComponent],
+  imports: [LandingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
